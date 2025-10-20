6c3c0425de72d6d187e186e061b2cf624bf8f666
pc Hải <pc Hi@HI>
2025-10-20 18:59:03 +0700

    Add project files.

---
+++ Scripts/thanhngu.js
@@ -0,0 +1,5 @@
+﻿function hienThiLoiChao() {
+    let message = "Chào mừng bạn đến với JavaScript!";
+    document.getElementById("loiChao").innerHTML = message;
+    console.log("Hàm hienThiLoiChao vừa được gọi!");
+}
